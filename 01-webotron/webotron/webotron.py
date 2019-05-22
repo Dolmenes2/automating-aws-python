83f586033c2d8107739638cf4468ca5f69ed0ecf
Dolmenes2 <dolmenes2@hotmail.com>
2019-05-22 16:23:33 +0100

    Output website host when syncing

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -12,7 +12,6 @@ Webotron automates the process of deploying static websites on aws.
 - Configure a Content Delivery Network and SSL with AWS
 """
 
-
 import boto3
 import click
 
@@ -26,48 +25,49 @@ bucket_manager = None
 @click.group()
 @click.option('--profile', default=None, help="Use a given AWS profile.")
 def cli(profile):
-  """Webotron deploys websites to AWS."""
-  global session, bucket_manager
-  
-  session_cfg = {}
-  if profile:
-    session_cfg['profile_name'] = profile
-  
-  session = boto3.Session(**session_cfg)
-  bucket_manager = BucketManager(session)
+    """Webotron deploys websites to AWS."""
+    global session, bucket_manager
+
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+
+        session = boto3.Session(**session_cfg)
+        bucket_manager = BucketManager(session)
 
 
 @cli.command('list-buckets')
 def list_buckets():
-  """List all s3 buckets."""
-  for bucket in bucket_manager.all_buckets():
-   print(bucket)
+    """List all s3 buckets."""
+    for bucket in bucket_manager.all_buckets():
+        print(bucket)
 
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-  """List objects in an s3 bucket."""
-  for obj in bucket_manager.all_objects(bucket): print(obj)
-  
+    """List objects in an s3 bucket."""
+    for obj in bucket_manager.all_objects(bucket): print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-  """Create and configure S3 bucket."""
-  s3_bucket = bucket_manager.init_bucket(bucket)
-  bucket_manager.set_policy(s3_bucket)
-  bucket_manager.configure_website(s3_bucket) 
-  return
+    """Create and configure S3 bucket."""
+    s3_bucket = bucket_manager.init_bucket(bucket)
+    bucket_manager.set_policy(s3_bucket)
+    bucket_manager.configure_website(s3_bucket)
+    return
 
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-  """Sync contents of PATHNAME to BUCKET."""
-  bucket_manager.sync(pathname, bucket)
-    
+    """Sync contents of PATHNAME to BUCKET."""
+    bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
+
 
 if __name__ == '__main__':
-	cli()
+	        cli()
